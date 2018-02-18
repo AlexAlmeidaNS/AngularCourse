1928d5a38cb6dd9660fa6379e3114ce7bca5c2a4
Alex Almeida <alex.b.almeida@gmail.com>
2018-02-18 16:55:04 -0400

    End of session 3

--- recipehub/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipehub/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+
+  	new Recipe('Ratatouille', 'This one comes from a movie', 'https://i.pinimg.com/736x/43/43/f9/4343f99002d350663f6b1c56d825c6f4--misto-ratatouille.jpg')
+  	new Recipe('Green Juice', 'This is a healthy choice!', 'https://cdn.cpnscdn.com/static.coupons.com/ext/kitchme/images/recipes/600x400/healthy-green-juice_26121.jpg')
+
+
+  ];
 
   constructor() { }
 
